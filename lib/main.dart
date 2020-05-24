ea5205fb1150d25ab1e95d38083e7b1dd546f02a
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-24 19:07:46 -0300

    Info screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login_screen.dart';
+import 'screens/infos_screen.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      
-    )
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portal Fiap Professor',
+      theme: new ThemeData(
+        primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
+        fontFamily: 'Arial',
+      ),
+      initialRoute: '/infos',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/infos': (context) => InfosScreen(),
+      },
+    );
+  }
 }
---
+++ lib/screens/infos_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfosScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SI Info'),
+        backgroundColor: Color(0xFFed145b),
+      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(30),
+        crossAxisSpacing: 25,
+        mainAxisSpacing: 25,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: const Text(
+                "1° SI",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFed145b),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFed145b),
+                width: 5,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0),
+                bottomLeft: Radius.circular(25.0),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: const Text(
+                "2° SI",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFed145b),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFed145b),
+                width: 5,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0),
+                bottomLeft: Radius.circular(25.0),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: const Text(
+                "3° SI",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFed145b),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFed145b),
+                width: 5,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0),
+                bottomLeft: Radius.circular(25.0),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: const Text(
+                "4° SI",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFed145b),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFed145b),
+                width: 5,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0),
+                bottomLeft: Radius.circular(25.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
