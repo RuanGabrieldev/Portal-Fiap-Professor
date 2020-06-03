9174f5c7a832b361fe6cd7eb647ac32c2a84a171
GustavoNovaes-BR <gusttavonovaes1@gmail.com>
2020-06-02 21:22:22 -0300

    incluindo tela de pesquisa de trabalho

---
+++ lib/screens/pesquisa_trabalho_screen.dart
@@ -0,0 +1,366 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TelaEntreTrabalho extends StatefulWidget {
+  @override
+  _TelaEntreTrabalhoState createState() => _TelaEntreTrabalhoState();
+}
+
+class _TelaEntreTrabalhoState extends State<TelaEntreTrabalho> {
+  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text("Cadastro de Trabalho/Prova"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body:SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 00.0),
+                child: Container(
+                  child: Text(
+                    "Welcome Flavio",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, left: 00.0),
+                child: Container(
+                  height: 60.0,
+                  width: 300.0,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.green,
+                    child: Text(
+                      "Cadastrar novo trabalho",
+                      style: TextStyle(color: Colors.white, fontSize: 19.0),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, left: 00.0),
+                child: Text(
+                  "Pesquisar trabalho",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(
+                            Icons.code,
+                            color: Colors.deepPurple,
+                          ),
+                          labelText: 'Digite o codigo',
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: 'Exemplo: 01234',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          if (int.parse(value) < 0) {
+                            return "Não pode ser numero negativo";
+                          }
+                          return null;
+                        },
+                        //adicioanando o valor para nome
+                        onSaved: (value) {
+                          //onde salvar
+                        },
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: DropdownButtonFormField(
+                        // Aqui estou cadastrando as opções
+                        items: [
+                          "2018",
+                          "2019",
+                          "2020",
+                          "2021"
+                        ]
+                            .map((label) => DropdownMenuItem(
+                          //Coloamos a linha a baixo para montar o corpo e mostrar o texto de umas das opções
+                          child: Text(label),
+                          //salvando o valor
+                          value: label,
+                        ))
+                            .toList(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(Icons.date_range, color: Colors.deepPurpleAccent,),
+                          labelText: "Informe o ano",
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: "Escolha uma das opções",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          if (value == null) {
+                            return "Favor selecionar um ano";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          //deve ser salvar
+                        },
+                        //segue a mesma ideia do onSaved
+                        onChanged: (value) {
+                          //deve ser salvo
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: DropdownButtonFormField(
+                        // Aqui estou cadastrando as opções
+                        items: [
+                          "Sistema de informação",
+                          "Engenharia",
+                          "Biologia"
+                        ]
+                            .map((label) => DropdownMenuItem(
+                          //Coloamos a linha a baixo para montar o corpo e mostrar o texto de umas das opções
+                          child: Text(label),
+                          //salvando o valor
+                          value: label,
+                        ))
+                            .toList(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(Icons.border_all, color: Colors.deepPurpleAccent,),
+                          labelText: "Informe o curso",
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: "Escolha uma das opções",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          if (value == null) {
+                            return "Favor selecionar um curso";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          //deve ser salvar
+                        },
+                        //segue a mesma ideia do onSaved
+                        onChanged: (value) {
+                          //deve ser salvo
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: DropdownButtonFormField(
+                        // Aqui estou cadastrando as opções
+                        items: [
+                          "3SIR",
+                          "3SIS",
+                          "3SIA",
+                          "3SIT"
+                        ]
+                            .map((label) => DropdownMenuItem(
+                          //Coloamos a linha a baixo para montar o corpo e mostrar o texto de umas das opções
+                          child: Text(label),
+                          //salvando o valor
+                          value: label,
+                        ))
+                            .toList(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(Icons.bookmark, color: Colors.deepPurpleAccent,),
+                          labelText: "Informe uma turma",
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: "Escolha uma das opções",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          if (value == null) {
+                            return "Favor selecionar uma turma";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          //deve ser salvar
+                        },
+                        //segue a mesma ideia do onSaved
+                        onChanged: (value) {
+                          //deve ser salvo
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: DropdownButtonFormField(
+                        // Aqui estou cadastrando as opções
+                        items: [
+                          "Flutter",
+                          "java",
+                          "C#"
+                        ]
+                            .map((label) => DropdownMenuItem(
+                          //Coloamos a linha a baixo para montar o corpo e mostrar o texto de umas das opções
+                          child: Text(label),
+                          //salvando o valor
+                          value: label,
+                        ))
+                            .toList(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(Icons.book, color: Colors.deepPurpleAccent,),
+                          labelText: "Informe a diciplina",
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: "Escolha uma das opções",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          if (value == null) {
+                            return "Favor selecionar uma diciplina";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          //deve ser salvar
+                        },
+                        //segue a mesma ideia do onSaved
+                        onChanged: (value) {
+                          //deve ser salvo
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            borderSide:
+                            BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          icon: Icon(
+                            Icons.border_color,
+                            color: Colors.deepPurple,
+                          ),
+                          labelText: 'Digite o tema',
+                          labelStyle: TextStyle(color: Colors.white),
+                          hintText: 'Exemplo: Nesse trabalho vamos abordar...',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent, fontSize: 20.0),
+                        validator: (value) {
+                          return null;
+                        },
+                        //adicioanando o valor para nome
+                        onSaved: (value) {
+                          //onde salvar
+                        },
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0, left: 00.0),
+                      child: Container(
+                        height: 60.0,
+                        width: 300.0,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          color: Colors.pinkAccent,
+                          child: Text(
+                            "Pesquisar",
+                            style: TextStyle(color: Colors.white, fontSize: 19.0),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
